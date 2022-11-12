f7cd651c41d6a6c5debdef4e513088218b18ce6a
Your Name Main <you@example.com>
2022-11-11 21:51:24 -0800

    login regiter form added

--- src/Pages/Authentication/Login/Login.js
+++ src/Pages/Authentication/Login/Login.js
@@ -1,11 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import loging from '../../../Assets/images/login.png'
 
 const Login = () => {
+    const handleSubmitLogin=event =>{
+        event.preventDefault();
+        const form =event.target
+        const email=form.email.value
+        const password=form.password.value
+      console.log(email,password);
+    }
     return (
-        <div>
-            <h2 className="text-3xl">This is Log in page</h2>
+        <div className="hero  bg-base-200">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+
+    <div>
+    <figure>
+        <img src={loging} alt="" />
+    </figure>    
+    </div> 
+   
+    <div className="card flex-shrink-0 w-full  max-w-sm shadow-2xl bg-base-100">
+      <form onSubmit={handleSubmitLogin} className="card-body">
+            <div className="text-center my-8">
+                <h1 className="text-5xl font-bold">Login now!</h1>
+            </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" name='email' placeholder="email" className="input input-bordered" />
         </div>
-    );
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" name='password' placeholder="password" className="input input-bordered" />
+          <label className="label">
+            <Link  className="label-text-alt link link-hover">Forgot password?</Link>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white font-bold">Login</button>
+        </div>
+      </form>
+      <div className='text-center mb-6'>
+        <p>You have an no account <Link to='/register' className='underline text-primary'>Please Register</Link></p>
+      </div>
+    </div>
+  </div>
+        </div>
+    ); 
 };
 
 export default Login;
--- src/Pages/Authentication/Register/Register.js
+++ src/Pages/Authentication/Register/Register.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import login from '../../../Assets/images/login.png'
 
 const Register = () => {
+    const handleSubmitLogin=event =>{
+        event.preventDefault();
+        const form =event.target
+        const name=form.name.value
+        const email=form.email.value
+        const password=form.password.value
+       console.log(name,email,password);
+    }
     return (
-        <div>
-            <h2 className='text-2xl'>This Is register</h2>
+        <div className="hero  bg-base-200">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+
+    <div>
+    <figure>
+        <img src={login} alt="" />
+    </figure>    
+    </div> 
+   
+    <div className="card flex-shrink-0 w-full  max-w-sm shadow-2xl bg-base-100">
+      <form onSubmit={handleSubmitLogin} className="card-body">
+          <div className="text-center my-8">
+              <h1 className="text-5xl font-bold">Login now!</h1>
+            </div>
+
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" name='email' placeholder="email" className="input input-bordered" />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" name='password' placeholder="password" className="input input-bordered" />
+          <label className="label">
+            <Link  className="label-text-alt link link-hover">Forgot password?</Link>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white font-bold">Register</button>
+        </div>
+      </form>
+      <div className='text-center mb-6'>
+        <p>Already have an account <Link to='/login' className='underline text-primary'>Log in</Link></p>
+      </div>
+    </div>
+  </div>
         </div>
     );
 };
